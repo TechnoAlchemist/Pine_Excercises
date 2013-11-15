ffb900317125d84ccc4bf2a602a26bd7b829abec
Gabe Lafontant <glafonta@gmail.com>
2013-11-15 11:14:38 -0500

    using arrays to print song lyrics

---
+++ bottles_of_beer.rb
@@ -0,0 +1,15 @@
+(1..99).to_a.reverse.each do |beer|
+	puts "#{beer} bottles of beer on the wall\n"
+	puts "#{beer} bottles of beer!\n"
+	puts "You take one down and pass it around...\n"
+	# puts "#{beer} bottles of beer on the wall!"
+end
+	
+
+
+# while bottles_of_beer != 0
+# 	puts "#{bottles_of_beer} bottles of beer on the wall\n"
+# 	puts "#{bottles_of_beer} bottles of beer!\n"
+# 	puts "You take one down and pass it around...\n"
+# 	puts "#{bottles_of_beer - 1} bottles of beer on the wall!"
+# end
