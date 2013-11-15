7e5f282596166d067649eb794c25ff1ddc5db276
Gabe Lafontant <glafonta@gmail.com>
2013-11-15 12:11:13 -0500

    closer but still haven't figured an exit

--- deaf_grandma.rb
+++ deaf_grandma.rb
@@ -1,8 +1,13 @@
 puts "Grandma just awoke from her nap\n" 
 puts "What would you like to discuss with her?"
-utterance = gets.chomp
+random_year = rand(1930..1950)
+end_conversation_with_grandma = "BYE"
 
-until utterance == "BYE"
-	if utterance != utterance.upcase
-		puts "HUH? SPEAK UP, SONNY" 
+while end_conversation_with_grandma  == "BYE"
+	speech = gets.chomp.to_s
+	if speech != speech.to_s.upcase
+		puts "HUH? SPEAK UP, SONNY"
+	else 
+		puts "NO, NOT SINCE #{random_year}"
+	end 
 end
