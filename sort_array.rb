9bf71774dadbb75f98b23c28f1353717447ecdc9
Gabe Lafontant <glafonta@gmail.com>
2013-11-18 17:13:41 -0500

    I have no idea why this is not fucking working

--- sort_array.rb
+++ sort_array.rb
@@ -1,16 +1,31 @@
 continous_words = "yes"
-range_of_words = []
+words = []
+puts "Please enter some random words for me to sort"
 
 while continous_words == "yes"
-	puts "Please enter some random words"
-	random_words = gets.chomp.to_s
-	range_of_words << random_words
+	word = gets.chomp.to_s
 
-	if random_words == "exit"
-		range_of_words.pop
+	if word == ""	
 		break
 	end
+
+	words << word
 end
 
-range_of_words.sort!
-puts range_of_words
+puts "Here are the sorted words:"
+puts words.sort!
+
+# puts "Give me some words, and I will sort them:"
+# words = []
+
+# while true
+# 	word = gets.chomp
+# 	if word == ''
+# 		break
+# 	end
+
+# 	words << word
+# end
+
+# puts "Sweet! Here they are are, sorted:"
+# puts words.sort
