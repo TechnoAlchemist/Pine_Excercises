1fc93b23d4e5b22384b547790f31b8086104fd57
Gabe Lafontant <glafonta@gmail.com>
2013-11-18 17:40:25 -0500

    expirement with concat

--- table_contents_revisited.rb
+++ table_contents_revisited.rb
@@ -1,11 +1,18 @@
 table_of_contents = []
-chapters_names = ["Chapter1: Getting Started", "Chapter 2: Numbers", "Chapter 3: Letters"]
+chapter_names = ["Chapter1: Getting Started", "Chapter 2: Numbers", "Chapter 3: Letters"]
 page_numbers = ["page 1", "page 9", "page 13"]
+really_specific = chapter_names.concat(page_numbers)
+puts really_specific
 
-chapters_names.each do |chapter|
-	line_width = 30
-	puts chapter.ljust(line_width)
-end
+# chapter_names.each do |chapter|
+# 	line_width = 30
+# 	puts chapter.ljust(line_width)
+# end
+
+# page_numbers.each do |page|
+# 	line_width = 15
+# 	puts page.rjust(line_width)
+# end
 
 # some_array.each do |name|
 # 	line_width = 30
