870d1c142a3aa483f5265f16336ace9f33f7ad76
Gabe Lafontant <glafonta@gmail.com>
2013-11-18 17:18:35 -0500

    it was working fine the first time

--- sort_array.rb
+++ sort_array.rb
@@ -13,19 +13,5 @@ while continous_words == "yes"
 end
 
 puts "Here are the sorted words:"
-puts words.sort!
+puts words.sort
 
-# puts "Give me some words, and I will sort them:"
-# words = []
-
-# while true
-# 	word = gets.chomp
-# 	if word == ''
-# 		break
-# 	end
-
-# 	words << word
-# end
-
-# puts "Sweet! Here they are are, sorted:"
-# puts words.sort
