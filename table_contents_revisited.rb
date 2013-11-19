c922f5af69186c94721486aefd8a08d2650f37b6
Gabe Lafontant <glafonta@gmail.com>
2013-11-19 13:01:50 -0500

    final final commit

--- table_contents_revisited.rb
+++ table_contents_revisited.rb
@@ -1,18 +1,4 @@
-# table_of_contents = []
-# chapter_names = ["Chapter1: Getting Started", "page 1", "Chapter 2: Numbers", "Chapter 3: Letters"]
-# page_numbers = ["page 1", "page 9", "page 13"]
-# # really_specific = chapter_names.concat(page_numbers)
-# # puts really_specific
 
-# chapter_names.each do |chapter|
-# 	line_width = 30
-# 	puts chapter.ljust(line_width)
-# end
-
-# page_numbers.each do |page|
-# 	line_width = 15
-# 	puts page.rjust(line_width)
-# end
 require 'pry'
 
 title = "Table of Contents"
@@ -32,15 +18,6 @@ all_the_chapters.each_with_index do |chap, index|
 	line_width = 30
 	name, page = chap
 	chap_num = index + 1
-	# puts "#{name}"
-	# puts "#{page}"
-	# puts "Chapter #{chap_num}"
-	# puts chapter + 1 
-	# binding.pry
-	puts name.ljust(30) + page.rjust(30)
 	
-
-
-
-	# puts "Chapter #{chap_num}".ljust(line_width) + page.rjust(line_width/2)
+	puts name.ljust(30) + page.rjust(30)
 end
